9e8cb2b756635d1088d9ccb2164a7dac16d7e104
Lan Yang <ireneyanglan@gmail.com>
2015-02-27 21:08:45 -0500

    Update getTwitterData.py

--- getTwitterData.py
+++ getTwitterData.py
@@ -1,10 +1,10 @@
 import tweepy
 import sys
 
-consumer_key = 'd5qhU69TNYmuT608SPk7Wy5VZ'
-consumer_secret = 'QuJziBDzwwss8P5WatwdZigCvhB5r2tphRbvWGOaKZ3RcY1cil'
-access_token = '3019121609-3U2LQvT4XNYuWeX0SkSW7WdKhQlnF5JqP9lIemh'
-access_token_secret = '3Q9jVLQaULyu2Kj0QEYgxzVvqURYZetTl1naOCi6ycxZl'
+consumer_key = 'xxx'
+consumer_secret = 'xxx'
+access_token = 'xxx'
+access_token_secret = 'xxx'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
